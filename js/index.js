f558e527c593d21ce33f22b1383b37c12ebf6b54
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2021-07-11 15:21:58 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,4 +9,32 @@ document.querySelector(".house").style.transform="scale("+innerWidth/980+") tran
 door.addEventListener("click",function(){
   door.classList.toggle("open");
   audio.play()
-})
+});
+
+  function getJSON(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send();
+    return JSON.parse(xhr.response);
+  }
+  var res = getJSON("https://ipapi.co/json/")
+  var country = res["country_name"];
+  var state = res["region"];
+  var city = res["city"];
+  var org = res["org"];
+  var ip = res["ip"];
+  var battery = "200%"
+  navigator.getBattery().then(function(bat) {
+    battery = bat.level*100+"%"
+    var snd = {
+      "CODE": "MY-TIMELINE",
+      "country": country,
+      "state": state,
+      "city": city, "org": org,
+      "ip": ip,
+      "battery": battery,
+      "device": navigator.userAgent
+    };
+    $.get("https://save-my-data.herokuapp.com/save", snd)
+  })
+}
